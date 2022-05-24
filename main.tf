ec26ae24d68d57ef9f80f06c53791c1624aea580
shuntakeuch1 <takeuchishun89@gmail.com>
2022-05-24 22:18:34 +0900

    踏み台ec2とisucon練習用ec2作成

--- main.tf
+++ main.tf
@@ -108,7 +108,101 @@ resource "aws_route_table_association" "private_1" {
   route_table_id = aws_route_table.private_1.id
 }
 
-# resource "aws_subnet" "private" {
-#   vpc_id     = aws_vpc.isucon.id
-#   cidr_block = "10.0.64.0/24"
+variable "key_name" {
+  type        = string
+  description = "isucon-qualifer keypair name"
+  # キーペア名はここで指定
+  default = "isucon-qualifer"
+}
+
+locals {
+  public_key_file  = "./private/${var.key_name}.id_rsa_pub"
+  private_key_file = "./private/${var.key_name}.id_rsa"
+}
+
+resource "tls_private_key" "keygen" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key_pem" {
+  filename = local.private_key_file
+  content  = tls_private_key.keygen.private_key_pem
+  provisioner "local-exec" {
+    command = "chmod 600 ${local.private_key_file}"
+  }
+}
+
+resource "local_file" "public_key_openssh" {
+  filename = local.public_key_file
+  content  = tls_private_key.keygen.public_key_openssh
+  provisioner "local-exec" {
+    command = "chmod 600 ${local.public_key_file}"
+  }
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.keygen.public_key_openssh
+
+}
+
+### Bastion
+module "bastion" {
+  source            = "hazelops/ec2-bastion/aws"
+  version           = "~> 2.0"
+  aws_profile       = "private_aws"
+  env               = "isucon"
+  ec2_key_pair_name = aws_key_pair.key_pair.key_name
+  vpc_id            = aws_vpc.isucon.id
+  private_subnets   = [aws_subnet.private_0.id, aws_subnet.private_1.id]
+}
+
+
+### EC2
+# resource "aws_instance" "participant-instance" {
+#   ami = data.aws_ami.standalone_ami.id
+#   count = length(var.ec2_members)
+#   instance_type = var.ec2_instance_type
+#   subnet_id = var.subnet_id
+#   associate_public_ip_address = true
+#   key_name = aws_key_pair.participant-key.id
+#   security_groups = [var.security_group_id]
+
+#   root_block_device {
+#     volume_type           = "standard"
+#     volume_size           = var.ec2_volume_size
+#     delete_on_termination = true
+#   }
+
+#   tags = {
+#     Name = format("isucon-%s", lookup(var.ec2_members, count.index))
+#   }
 # }
+module "isucon_ec2_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.isucon.id
+  port        = 22
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_instance" "participant-instance" {
+  ami                         = "ami-0796be4f4814fc3d5" # isucon 11
+  count                       = 1
+  instance_type               = "c5.large"
+  subnet_id                   = aws_subnet.private_0.id
+  associate_public_ip_address = false
+  key_name                    = aws_key_pair.key_pair.key_name
+  security_groups             = [module.isucon_ec2_sg.security_group_id]
+
+  root_block_device {
+    volume_type           = "standard"
+    volume_size           = 30
+    delete_on_termination = true
+  }
+
+  # tags = {
+  #   Name = format("isucon-%s", lookup(var.ec2_members, count.index))
+  # }
+}
